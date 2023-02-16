9b516ee5584725674bce13d0b4bcfccb3b6a5973
mgckaled <marcelgckaled@gmail.com>
2023-02-16 17:32:56 -0300

    feat: change title page to avoid stop countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -48,10 +48,16 @@ export function Home() {
 	const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
 	useEffect(() => {
+		let interval: ReturnType<typeof setInterval>
+
 		if (activeCycle) {
-			setInterval(() => {
+			interval = setInterval(() => {
 				setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
 			}, 1000)
+		}
+
+		return () => {
+			clearInterval(interval)
 		}
 	}, [activeCycle])
 
@@ -67,6 +73,7 @@ export function Home() {
 
 		setCycles((state) => [...state, newCycle])
 		setActiveCycleId(id)
+		setAmountSecondsPassed(0)
 
 		reset()
 	}
@@ -79,6 +86,12 @@ export function Home() {
 
 	const minutes = String(minutesAmount).padStart(2, '0')
 	const seconds = String(secondsAmount).padStart(2, '0')
+
+	useEffect(() => {
+		if (activeCycle) {
+			document.title = `${minutes}:${seconds}`
+		}
+	}, [minutes, seconds, activeCycle])
 
 	const task = watch('task')
 	const isSubmitDisable = !task
