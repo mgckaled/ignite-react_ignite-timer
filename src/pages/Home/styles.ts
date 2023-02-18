82ab365ba038e9bbaf4e5ec2435c75fe0a39aa70
mgckaled <marcelgckaled@gmail.com>
2023-02-18 10:05:41 -0300

    feat: handle with interrupt cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -26,6 +26,7 @@ export const FormContainer = styled.div`
 
 	font-size: 1.125rem;
 	font-weight: bold;
+
 	flex-wrap: wrap;
 `
 
@@ -37,9 +38,9 @@ const BaseInput = styled.input`
 
 	font-weight: bold;
 	font-size: 1.125rem;
-	color: ${(props) => props.theme['gray-100']};
-
 	padding: 0 0.5rem;
+
+	color: ${(props) => props.theme['gray-100']};
 
 	&:focus {
 		box-shadow: none;
@@ -89,7 +90,7 @@ export const Separator = styled.div`
 	justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
 	width: 100%;
 	border: 0;
 	padding: 1rem;
@@ -97,21 +98,34 @@ export const StartCountdownButton = styled.button`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+
+	color: ${(props) => props.theme['gray-100']};
+
 	gap: 0.5rem;
 
 	font-weight: bold;
 
 	cursor: pointer;
 
-	background: ${(props) => props.theme['green-500']};
-	color: ${(props) => props.theme['gray-100']};
-
 	&:disabled {
 		opacity: 0.7;
 		cursor: not-allowed;
 	}
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+	background: ${(props) => props.theme['green-500']};
+	color: ${(props) => props.theme['gray-100']};
 
 	&:not(:disabled):hover {
 		background: ${(props) => props.theme['green-700']};
+	}
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+	background: ${(props) => props.theme['red-500']};
+
+	&:not(:disabled):hover {
+		background: ${(props) => props.theme['red-700']};
 	}
 `
